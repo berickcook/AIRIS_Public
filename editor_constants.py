438920bc97d34c4efcf983932b8dcea0484cc886
berickcook <berickcook@gmail.com>
2019-05-29 08:37:16 -0800

    Cleaned up formatting

--- editor_constants.py
+++ editor_constants.py
@@ -1,10 +1,7 @@
-
-############ PUZZLE GAME EDITOR #################
-
 GAME_SCREEN_SIZE = (640, 608)
 
-#MAP_SIZE = (640, 480) # original
-GAME_MAP_SIZE = (640, 480) # GAME_MAP_SIZE is the number of pixels the entire game map is wide and tall
-GAME_MAP_GRID = (20, 15) # GAME_MAP_GRID is the number of of tiles the game is wide and tall
-GAME_MAP_START = (0, 64) # GAME_MAP_START is the coordinates on the screen where the top left of the game map is displayed
-GAME_POS_SIZE = (int(GAME_MAP_SIZE[0] / GAME_MAP_GRID[0]), int(GAME_MAP_SIZE[1] / GAME_MAP_GRID[1])) # pixel dim. of position
+# MAP_SIZE = (640, 480) # original
+GAME_MAP_SIZE = (640, 480)  # GAME_MAP_SIZE is the number of pixels the entire game map is wide and tall
+GAME_MAP_GRID = (20, 15)  # GAME_MAP_GRID is the number of of tiles the game is wide and tall
+GAME_MAP_START = (0, 64)  # GAME_MAP_START is the screen coordinates where the top left of the game map is displayed
+GAME_POS_SIZE = (int(GAME_MAP_SIZE[0] / GAME_MAP_GRID[0]), int(GAME_MAP_SIZE[1] / GAME_MAP_GRID[1]))  # pixel scale
